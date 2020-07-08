3d5afd454fbe244cc1e230de21040d9f87951ca6
isaak-treat <isaaktreaty@gmail.com>
2020-07-07 18:13:07 -0600

    Updated some CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Marvel API Angular Application';
+  title = 'Marvel Tour of Heroes';
 }
